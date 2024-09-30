df367307fb626cc57926178aba6f10f50f2052c9
GiroFrm <girofram844@gmail.com>
2024-09-30 13:17:55 +1000

    feat: Add event listener to crete grid button
    - Select the button with id 'create-grid' and assign it to btn_createGrid.
    - Add a click event listener to btn_createGrid
    - Prompt the user to enter the number of squares per side when the button is clicked

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
 
 // button to call prompt and ask for number of squares
+let numberSquares = 8;
 
+
+
+const btn_createGrid = document.querySelector("#create-grid");
+    btn_createGrid.addEventListener("click", ()=>{
+       numberSquares =   prompt("number of squares per side");
+      console.log(numberSquares);
+    })
 // create hover effect when the mouse hover on top of each square it will fill with color the square
 // randomize the RBGA values with each interactio
 // implement progressive darkening effect by 10% reaching full black.
 
 
-let numberSquares = 8;
+
 let totalsquares = numberSquares * numberSquares;
 // calculate the size of each square as size of width (960px in this case)
 let sizeSquare = 960 / numberSquares;
 console.log(sizeSquare);
-// 960px / 16 squares per side = 60px width x 60 px height
 
 const containerSquares = document.querySelector(".container");
 for(let i = 0; i<totalsquares; i++) {
