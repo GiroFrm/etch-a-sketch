222b6bb2ae6916c3053ac41567702097cde1c46c
GiroFrm <girofram844@gmail.com>
2024-09-30 13:37:10 +1000

    feat: Add createSquares function call inside click listener for #create-grid button

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 
 // button to call prompt and ask for number of squares
 let numberSquares = 8;
+const containerSquares = document.querySelector(".container");
 
 createSquares(8);
 
 const btn_createGrid = document.querySelector("#create-grid");
     btn_createGrid.addEventListener("click", ()=>{
-       numberSquares =   prompt("number of squares per side");
-      console.log(numberSquares);
+       numberSquares =   +prompt("number of squares per side");
+      console.log(typeof (numberSquares));
+      createSquares(numberSquares);
     })
 // create hover effect when the mouse hover on top of each square it will fill with color the square
 // randomize the RBGA values with each interactio
@@ -20,7 +22,7 @@ function createSquares(numberSquares){
     let sizeSquare = 960 / numberSquares;
     console.log(sizeSquare);
 
-    const containerSquares = document.querySelector(".container");
+   
     for(let i = 0; i<totalsquares; i++) {
         const newSquare = document.createElement("div");
             newSquare.style.width = sizeSquare+"px";
